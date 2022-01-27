8e2f7131a361455a11a7affad0a2b201c14dc485
Damfler <gashuk2002@yandex.ru>
2022-01-27 21:17:15 +0300

    add: Controller

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\PostFormRequest;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::query()->orderBy('created_at', 'DESC')->paginate(5);
+
+        return view('admin.posts.dashboard', [
+            'posts' => $posts,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.posts.dashboard_edit', []);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function store(PostFormRequest $request)
+    {
+        $data = $request->validated();
+
+        if ($request->has('thumbnail')) {
+            $thumbnail = str_replace('public/posts', '', $request->file('thumbnail')->store('public/posts'));
+            $data['thumbnail'] = $thumbnail;
+        }
+
+        $post = Post::create($data);
+
+        return redirect(route('admin.posts.index'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = Post::query()->findOrFail($id);
+
+        return view('admin.posts.dashboard_edit', [
+            'post' => $post,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function update(PostFormRequest $request, $id)
+    {
+        $post = Post::query()->findOrFail($id);
+
+        $data = $request->validated();
+
+        if ($request->has('thumbnail')) {
+            $thumbnail = str_replace('public/posts', '', $request->file('thumbnail')->store('public/posts'));
+            $data['thumbnail'] = $thumbnail;
+        }
+
+        $post->update($data);
+
+        return redirect(route('admin.posts.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Post::destroy($id);
+
+        return redirect(route('admin.posts.index'));
+    }
+}
